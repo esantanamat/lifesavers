10f6be3e9186356bde70afafbcf3f52f2751b341
esantanamat <esantanamat@gmail.com>
2025-04-02 23:53:13 -0400

    mealprepcalc bash script

---
+++ usefulscripts/mealprepcalculator.sh
@@ -0,0 +1,24 @@
+echo "What's the total amount in grams?"
+read weight
+echo "How many grams per serving (check your macros)?"
+read servingsize
+result=$(( weight / servingsize))
+echo "This gives you about $result servings for this meal prep"
+if [ "$result" -gt 4 ]; then
+echo "This exceeds the recommend amount of meal prep days (4), consider cooking less next time"
+fi
+echo "Weights as you serve:"
+counter=0
+while [ "$weight" -gt 0 ] && [ "$counter" -lt 4 ]; do
+weight=$((weight - servingsize))
+counter=$(( counter + 1 ))
+if [ "$weight" -lt 0 ]; then
+echo "$((weight + servingsize)) left as excess (storage)" 
+break
+fi
+echo "After serving $counter: $weight g"
+done
+
+if [ $weight -gt 0 ]; then
+echo "$weight g for storage"
+fi
